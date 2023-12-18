0a9006bdb55f374c4940c8d7a11a3ab48f4c3934
l2o0v2e3 <153995795+l2o0v2e3@users.noreply.github.com>
2023-12-18 23:25:39 +0800

    Add files via upload

--- thirdAnniversary/js/swiper.js
+++ thirdAnniversary/js/swiper.js
@@ -1,11 +1,11 @@
-var swiper = new Swiper('.swiper-container-lovexhj1', {
-    pagination: '.swiper-pagination-lovexhj1',
+var swiper = new Swiper('.swiper-container-lovehnl1', {
+    pagination: '.swiper-pagination-lovehnl1',
     paginationClickable: true,
     spaceBetween: 30,
 });
 
-var swiper = new Swiper('.swiper-container-lovexhj2', {
-    pagination: '.swiper-pagination-lovexhj2',
+var swiper = new Swiper('.swiper-container-lovehnl2', {
+    pagination: '.swiper-pagination-lovehnl2',
     effect: 'coverflow',
     grabCursor: true,
     centeredSlides: true,
@@ -19,10 +19,10 @@ var swiper = new Swiper('.swiper-container-lovexhj2', {
     }
 });
 
-var swiper = new Swiper('.swiper-container-lovexhj3', {
-    pagination: '.swiper-pagination-lovexhj3',
+var swiper = new Swiper('.swiper-container-lovehnl3', {
+    pagination: '.swiper-pagination-lovehnl3',
     effect: 'flip',
     grabCursor: true,
-    nextButton: '.swiper-button-next-lovexhj3',
-    prevButton: '.swiper-button-prev-lovexhj3'
+    nextButton: '.swiper-button-next-lovehnl3',
+    prevButton: '.swiper-button-prev-lovehnl3'
 });
